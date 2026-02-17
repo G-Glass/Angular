15a2f000c228f33b386dd8cb49b3ddd72277a027
Sataca <glassg767@gmail.com>
2026-02-17 18:52:53 +0200

    commit

--- dynamic-stc/src/app/app.ts
+++ dynamic-stc/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Titulo } from './titulo/titulo';
+import { Menu } from './menu/menu';
+import { Conteudo } from './conteudo/conteudo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Titulo,Menu,Conteudo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ dynamic-stc/src/app/conteudo/conteudo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conteudo',
+  standalone:true,
+  imports: [],
+  templateUrl: './conteudo.html',
+  styleUrl: './conteudo.css',
+})
+export class Conteudo {
+
+}
